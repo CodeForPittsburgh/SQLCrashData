78d81bacf72632b928c65be2e69cab56e1156378
mhowe0422 <mhowe0422@msn.com>
2016-10-20 13:35:44 -0400

    Rebuild for GitHub

--- Disclaimer.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo'<h3 class="text-center">Disclaimer</h3>';
-echo '<p>The Pittsburgh Bureau of Police csv file is processed every morning at 0300 for the previous day of the city\'s police incidents. The display is limited to the accuracy of the contents and the number of incidents provided by the police and might not include all incidents for the day. It is important that any decisions based on this data be confirmed using additional resources. As the city says, "The City of Pittsburgh has provided this information as a service. The City assumes no responsibility for the use of information posted on this site." Mark Howe and Sunil Mogadati are the current team. Tim Condello, Mark Howe, Andrew McGill, Andy Somerville are the Original Open Pittsburgh Project Team Members.</p>';
-echo '<p><a href="./CityDisclaimer.html" target="_blank"> City Disclaimer Public Access Level Comment</a></p>';
-echo '<h4  class="text-center">Open Pittsburgh © 2016 <a href="https://twitter.com/openpgh" target="_blank">@OpenPGH</a></h4>';
